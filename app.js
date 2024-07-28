e204d332c3b21c9816a8b484e658e7f6508b4c3d
Anees <aneesnafeesimmu@gmail.com>
2024-07-28 11:34:57 +0530

    api-issue

--- app.js
+++ app.js
@@ -15,33 +15,30 @@ mailChimp.setConfig({
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html")
     var email = req.body.email;
-    app.post("/", function (req, res) {
+    app.post("/", async function (req, res) {
         var user = req.body.username;
         var pwd = req.body.password;
         var email = req.body.email;
         console.log("Received data:", { user, pwd, email });
 
-    try {
-        mailChimp.lists.addListMember("14d988a883", {
-            email_address: email,
-            status: "subscribed",
-            merge_fields: {
-                FNAME: user,
-                LNAME: pwd
-            }
-        }).then((response) => {   // I was getting promsie {Pending} when doing console.log(response). So, I treid then and it worked. If you don't want to log then it'll work without then also
+        try {
+            const response = await mailChimp.lists.addListMember("14d988a883", {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: user,
+                    LNAME: pwd
+                }
+            });
+    
             console.log("Mailchimp response:", response);
             console.log("API Key used:", process.env.API_KEY);
-        });
-
-        // console.log("Mailchimp response:", response);
-        
-
-        res.sendFile(__dirname + "/result.html");
-    } catch (error) {
-        console.error("Error adding member to Mailchimp:", error);
-        res.status(500).send("Error adding member to Mailchimp");
-    }
+    
+            res.sendFile(__dirname + "/result.html");
+        } catch (error) {
+            console.error("Error adding member to Mailchimp:", error.response ? error.response.body : error);
+            res.status(500).send("Error adding member to Mailchimp");
+        }
         //then will take function 
         // .then((response) => {   // I was getting promsie {Pending} when doing console.log(response). So, I treid then and it worked. If you don't want to log then it'll work without then also
         //     console.log(response);
